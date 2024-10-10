574b66414ac920fd40ea8e7548927524f042b67d
Carolina Cordero <ccalvarez@gmail.com>
2024-10-10 17:28:06 -0600

    feat: balance de asset en fronted

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -2,12 +2,30 @@
 
 import Link from "next/link";
 import type { NextPage } from "next";
+import { formatEther } from "viem";
 import { useAccount } from "wagmi";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
+import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
+
+  const { data: vaultAsset } = useScaffoldReadContract({
+    contractName: "DimSumVault",
+    functionName: "asset",
+  });
+
+  const { data: yourTokenSymbol } = useScaffoldReadContract({
+    contractName: "STRANGE",
+    functionName: "symbol",
+  });
+
+  const { data: yourTokenBalance } = useScaffoldReadContract({
+    contractName: "STRANGE",
+    functionName: "balanceOf",
+    args: [connectedAddress],
+  });
 
   return (
     <>
@@ -15,32 +33,39 @@ const Home: NextPage = () => {
         <div className="px-5">
           <h1 className="text-center">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl font-bold">Dim Sum Vault</span>
           </h1>
           <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
             <p className="my-2 font-medium">Connected Address:</p>
             <Address address={connectedAddress} />
           </div>
-
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
         </div>
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
+        <div className="flex flex-col items-center bg-base-100 shadow-lg shadow-secondary border-8 border-secondary rounded-xl p-6 mt-24 w-full max-w-lg">
+          <div className="text-xl">
+            Your token balance:{" "}
+            <div className="inline-flex items-center justify-center">
+              {parseFloat(formatEther(yourTokenBalance || 0n)).toFixed(4)}
+              <span className="font-bold ml-1">yourTokenSymbol</span>
+            </div>
+          </div>
+          {/* Vendor Balances */}
+          <hr className="w-full border-secondary my-3" />
+          <div>
+            Vendor token balance:{" "}
+            <div className="inline-flex items-center justify-center">
+              vendorTokenBalance{/* {Number(formatEther(vendorTokenBalance || 0n)).toFixed(4)} */}
+              <span className="font-bold ml-1">vaultAsset</span>
+            </div>
+          </div>
+          <div>
+            {/* Vendor eth balance: {Number(formatEther(vendorEthBalance?.value || 0n)).toFixed(4)} */}
+            Vendor eth balance
+            <span className="font-bold ml-1">ETH</span>
+          </div>
+        </div>
+
+        {/* <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <BugAntIcon className="h-8 w-8 fill-secondary" />
@@ -63,7 +88,7 @@ const Home: NextPage = () => {
               </p>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   );
